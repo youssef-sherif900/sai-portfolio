5d218ffad5a5503a14c036483e8b7c7afe0d5b03
youssef-sherif900 <youssefsherif469@gmail.com>
2024-12-16 03:10:34 +0200

    auto focus on computer

--- components/CameraAnimation.jsx
+++ components/CameraAnimation.jsx
@@ -8,17 +8,31 @@ export default function CameraAnimation() {
     const { camera } = useThree();
     const [isAnimating, setIsAnimating] = useState(true);
     const startPos = useRef(new THREE.Vector3(0, 0, 0));
-    const targetPos = useRef(new THREE.Vector3(5, 8, 10));
+    const targetPos = useRef(new THREE.Vector3(-0.5, 4, -3));
+
+    const targetRotation = useRef(new THREE.Euler(0, 0, 0)); 
     
   
     useEffect(() => {
       camera.position.copy(startPos.current);
+
+      camera.rotation.copy(new THREE.Euler(0, 0, 0));
+
+      
+
     }, []);
   
     useFrame((state, delta) => {
       if (isAnimating) {
         // Move the camera towards the target position
         camera.position.lerp(targetPos.current, 0.1);
+
+
+              // Lerp the camera's rotation towards the target rotation
+      camera.rotation.x = THREE.MathUtils.lerp(camera.rotation.x, targetRotation.current.x, 0.1);
+      camera.rotation.y = THREE.MathUtils.lerp(camera.rotation.y, targetRotation.current.y, 0.1);
+      camera.rotation.z = THREE.MathUtils.lerp(camera.rotation.z, targetRotation.current.z, 0.1);
+
   
         // Check if the camera is close enough to the target position
         if (camera.position.distanceTo(targetPos.current) < 0.1) {
